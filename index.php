0a6f2442ab66f55df1a5d462c32384898d315cd6
DenisCis <loldenik812@gmail.com>
2023-08-25 13:14:39 +0300

    add

--- index.php
+++ index.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Ticket Land</title>
 </head>
 <body>
-    <div class="bg-slate-800 f-screen">
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </div>
+    <div class="index__main-page">
+        
+    </div>  
 </body>
 </html>  
